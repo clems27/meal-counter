50a7446033a3dbeea00af59cd7ce13af6d8d9b15
clement <clemohiomoje@gmail.com>
2019-04-14 18:56:25 +0100

    Made skeleton button components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App"> clement</div>;
+    return (
+      <div className="App">
+        <Heading />
+        <CounterButton title="Vegan" />
+        <CounterButton title="Halal" />
+        <CounterButton title="Veggie" />
+        <CounterButton title="Anything" />
+      </div>
+    );
   }
 }
-
+class Heading extends Component {
+  render() {
+    return <h1>Meal Counter</h1>;
+  }
+}
+class CounterButton extends Component {
+  render() {
+    return (
+      <div>
+        <button>curry</button>0
+      </div>
+    );
+  }
+}
 export default App;
