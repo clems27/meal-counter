9c9e2b96869fa68330ea6540b7b757042c638c48
clement <clemohiomoje@gmail.com>
2019-04-16 16:22:07 +0100

    Footer, scroll-bar, header-image and extra meal-button added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
 import React, { Component } from "react";
+import CounterButton from "./CounterButton.js";
+import Footer from "./Footer"
 import "./App.css";
 
 class App extends Component {
   render() {
+    const contactInfo = [
+      "64, Sebert Road London, E1 4UD",
+      "info@fadsmeal.com",
+      "0123 987654"
+    ];
     return (
       <div className="App">
         <Heading />
         <CounterButton title="Vegan" />
         <CounterButton title="Halal" />
         <CounterButton title="Veggie" />
-        <CounterButton title="Anything" />
+        <CounterButton title="Kosher" />
+        <CounterButton title="Meat" />
+        <CounterButton title="Sides" />
+        <CounterButton title="Drinks" />
+        <Footer contactInfo={contactInfo} />
       </div>
     );
   }
 }
+
 class Heading extends Component {
   render() {
-    return <h1>Meal Counter</h1>;
-  }
-}
-class CounterButton extends Component {
-  render() {
     return (
-      <div>
-        <button>curry</button>0
-      </div>
+      <header className="header">
+        <img
+          src="https://i.postimg.cc/JzKjv6jK/fads.png"
+          height="120px"
+          alt="logo"
+        />
+        <h1>Meal Counter</h1>
+        <div class="scroll-bar">
+          <h3>
+            Wether you just want to order for a single person or for a group!
+            This app makes ordering food for a group more easier{" "}
+          </h3>
+        </div>
+      </header>
     );
   }
 }
+
 export default App;
